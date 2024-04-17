0a2dca8669375715381136cb5dab699582bba64a
Yeasir Ahamed <yahamed007@gmail.com>
2024-04-17 18:14:02 +0600

    add color

--- day-one/sketch.js
+++ day-one/sketch.js
@@ -1,13 +1,42 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math');
+const random = require('canvas-sketch-util/random');
+var randomColor = require('randomcolor');
 
 const settings = {
   dimensions: [2048, 2048],
 };
 
 const sketch = () => {
+  const createFrid = () => {
+    let points = [];
+    const count = 25;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  };
+
+  const points = createFrid().filter(() => Math.random() > 0.5);
+  const margin = 300;
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'orange';
+    context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
+    points.forEach(([u, v]) => {
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
+      const color = randomColor();
+      context.beginPath();
+      context.arc(x, y, 25, 0, Math.PI * 2, false);
+      context.strokeStyle = color;
+      context.lineWidth = 20;
+      context.stroke();
+    });
   };
 };
 
